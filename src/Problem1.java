0b4714e4b79d385bf2cf377bee6f3b4272166db4
mingchen <chenming0317@gmail.com>
2022-08-07 01:06:42 -0700

    add local files

---
+++ src/Problem1.java
@@ -0,0 +1,6 @@
+public class Problem1 {
+    public static void main(string[] args)
+    {
+        System.out.println(("Hello, world"));
+    }
+}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " rock-paper-scissor" << endl;
+
+    return 0;
+}
